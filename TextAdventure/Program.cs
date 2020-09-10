77df412a49d8f0653457309760a43714d306df1b
Austin Hodge <austindh1992@gmail.com>
2020-09-10 13:31:50 -0500

    Added GenerateStats method

--- TextAdventure/Program.cs
+++ TextAdventure/Program.cs
@@ -8,7 +8,9 @@ namespace TextAdventure
     {
         static void Main(string[] args)
         {
-            DisplayStats(3, 2, 1);
+            int[] playerStats = GenerateStats();
+
+            DisplayStats(playerStats[0], playerStats[1], playerStats[2]);
         }
 
         public static void DisplayStats(int heart, int mind, int body)
@@ -16,6 +18,22 @@ namespace TextAdventure
             Console.WriteLine($"Heart: {heart}");
             Console.WriteLine($"Mind: {mind}");
             Console.WriteLine($"Body: {body}");
+        }
+
+        public static int[] GenerateStats()
+        {
+            int[] playerStats = new int[3];
+            Random rnd = new Random();
+
+            while (playerStats[0] + playerStats[1] + playerStats[2] != 5)
+            {
+                for (int i = 0; i < playerStats.Length; i++)
+                {
+                    playerStats[i] = rnd.Next(1, 3);
+                }
+            }
+
+            return playerStats;
         }
 
     }
