0880d956162b123158092e32a007aab065ac06cc
Austin Hodge <austindh1992@gmail.com>
2020-09-10 13:24:42 -0500

    Added DisplayStats method

---
+++ TextAdventure/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+
+namespace TextAdventure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DisplayStats(3, 2, 1);
+        }
+
+        public static void DisplayStats(int heart, int mind, int body)
+        {
+            Console.WriteLine($"Heart: {heart}");
+            Console.WriteLine($"Mind: {mind}");
+            Console.WriteLine($"Body: {body}");
+        }
+
+    }
+}
+
